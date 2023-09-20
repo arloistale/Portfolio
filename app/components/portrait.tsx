b69f2d38c0162cbfcb9dfa83c78caca480b3174f
Jonathan Lu <jonathanlu321@gmail.com>
2023-09-20 08:08:03 -0400

    Centering stuff, removing unnecessary stuff

--- app/components/portrait.tsx
+++ app/components/portrait.tsx
@@ -6,7 +6,7 @@ interface PortraitProps {
   
   const Portrait = ({className}: PortraitProps) => {
     return (
-      <div className={`${className} flex-shrink-0 relative w-40 h-40 lg:w-64 lg:h-64 border-16 border-white rounded-full egg-gradient egg-shadow mx-16`}>
+      <div className={`${className} flex-shrink-0 relative w-40 h-40 lg:w-64 lg:h-64 mx-16`}>
         <div className="absolute inset-0 bg-white rounded-full transform scale-150 "></div>
         <div className="absolute inset-0 bg-gray-300 rounded-full transform scale-105"></div>
         <Image
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className='px-4 lg:px-24 -mt-3 lg:mt-0 flex flex-col lg:flex-row relative w-full h-screen bg-hero-blue justify-center lg:justify-between items-center'>
         <div className='justify-center lg:mt-0 mb-16 lg:mb-0 mr-0 lg:mr-16 max-w-[75%]'>
           <h1 className='text-2xl lg:text-4xl font-bold text-hero'>Helping companies grow by developing high-impact web and mobile apps.</h1>
-          <Link href="#work" className="block lg:inline-block w-full lg:w-auto bg-hero-orange text-white py-4 px-6 rounded-lg mt-8 hover:brightness-90 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-hero-orange">See work</Link>
+          <Link href="#work" className="block lg:inline-block w-full lg:w-auto bg-hero-orange text-white text-center py-4 px-6 rounded-lg mt-8 hover:brightness-90 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-hero-orange">See work</Link>
         </div>
         <Portrait className='' />
       </div>
