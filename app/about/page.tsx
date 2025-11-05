9c95eca69e3b7ff95d9d78b0f46986ad57be6d32
Jonathan Lu <jonathanlu321@gmail.com>
2025-11-05 15:21:22 -0500

    Update about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,15 +10,15 @@ export default function About() {
         <div className="flex flex-col md:flex-row md:items-center">
           <Image src={"/jon.jpg"} alt={"Picture of Jon"} width={500} height={500} className="rounded-3xl"/>
           <div className="py-10 md:py-0 md:ml-10">
-            <p className="mb-4">Sky Egg Technologies is a development service and consultancy started by Jon Lu, a Brooklyn-based software engineer and writer.</p>
-            <p className="mb-4">He has built apps for many industries including games, film, healthcare, and real estate.</p>
-            <p className="mb-4">Some of his clients include:</p>
+            <p className="mb-4">Sky Egg Technologies is a development consultancy started by Jon Lu, a Brooklyn-based software engineer and writer.</p>
+            <p className="mb-4">He has built apps for many industries including radio mesh networks, games, film, healthcare, and real estate.</p>
+            <p className="mb-4">Clients:</p>
             <ul className='list-disc pl-5'>
               <li>Warby Parker</li>
               <li>Dots</li>
+              <li>gotenna</li>
               <li>ustwo</li>
             </ul>
-            <p className="mt-4">Thanks for visiting!</p>
             <p className="mt-8">
               <Link href="/#work" className="border border-gray-500 text-gray-500 rounded-md px-4 py-2 hover:bg-gray-500 hover:text-white transition duration-300 ease-in-out">See work</Link>
             </p>
