9ba41e94da589372bb5d47fa0defbe487cd18a2b
Jonathan Lu <jonathanlu321@gmail.com>
2025-11-05 15:05:06 -0500

    Updating things

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
       <footer className="bg-gray-100 text-gray-400 py-12 mt-8">
         <div className="container mx-auto text-center">
           <p className="mb-2 text-sm md:text-base">This website was built using Tailwind CSS and NextJS.</p>
-          <p className="text-sm md:text-base">Copyright &copy; 2023 Sky Egg Technologies LLC.</p>
+          <p className="text-sm md:text-base">Copyright &copy; 2025 Sky Egg Technologies LLC.</p>
         </div>
       </footer>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className='bg-hero-gradient-small lg:bg-hero-gradient px-4 lg:px-24 -mt-3 lg:mt-0 flex flex-col lg:flex-row relative w-full min-h-screen h-auto justify-start lg:justify-between items-center'>
         <div className='justify-center mt-16 lg:mt-0 mb-16 xs:mb-20 sm:mb-24 lg:mb-0 mr-0 lg:mr-16 max-w-[95%]'>
           <h1 className='text-2xl sm:text-4xl lg:text-5xl font-bold text-hero'>Helping companies grow by developing high-impact web and mobile apps.</h1>
-          <p className='font-thin text-base xs:text-lg md:text-xl mt-4'>A development service and consultancy started by Jon Lu, a Brooklyn-based engineer.</p>
+          <p className='font-thin text-base xs:text-lg md:text-xl mt-4'>A Brooklyn-based development service and consultancy.</p>
           <Link href="#work" className="block lg:inline-block w-full lg:w-auto text-base md:text-lg bg-hero-orange text-white text-center py-4 px-10 rounded-lg mt-8 hover:brightness-90 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-hero-orange">See work</Link>
         </div>
         <Portrait className='mx-0 lg:mx-16 mb-24 lg:mb-0' />
